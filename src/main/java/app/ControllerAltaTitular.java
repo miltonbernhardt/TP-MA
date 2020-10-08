8c35f53f6b62f8aea8d8ac5a05d441662cb38736
WeidmannCamila <camiweid@gmail.com>
2020-10-08 18:18:31 -0300

    Modificacion

--- src/main/java/app/ControllerAltaTitular.java
+++ src/main/java/app/ControllerAltaTitular.java
@@ -1,4 +1,18 @@
 package app;
 
-public class ControllerAltaTitular {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ControllerAltaTitular implements Initializable{
+
+    @FXML private
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
 }
