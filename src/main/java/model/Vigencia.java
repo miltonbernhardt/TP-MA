19648f7e9c0b6346b0396046a43da3449367693c
MatiasWettstein <matiasjwettstein@gmail.com>
2020-10-08 14:05:36 -0300

    creada la clase Vigencia

---
+++ src/main/java/model/Vigencia.java
@@ -0,0 +1,25 @@
+package model;
+
+import java.util.Date;
+
+public class Vigencia {
+
+    public Date fechaVencimiento;
+    public int vigencia;
+
+    public Date getFechaVencimiento() {
+        return fechaVencimiento;
+    }
+
+    public void setFechaVencimiento(Date fechaVencimiento) {
+        this.fechaVencimiento = fechaVencimiento;
+    }
+
+    public int getVigencia() {
+        return vigencia;
+    }
+
+    public void setVigencia(int vigencia) {
+        this.vigencia = vigencia;
+    }
+}
