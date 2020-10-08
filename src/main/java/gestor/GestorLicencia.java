2405180b950140a3684e2689880000911183474f
AgustinGH <38263298+AgustinGHunzicker@users.noreply.github.com>
2020-10-08 20:39:50 -0300

    - GestorLicencia.java - GestorTitular.java

--- src/main/java/gestor/GestorLicencia.java
+++ src/main/java/gestor/GestorLicencia.java
@@ -1,10 +1,15 @@
 package gestor;
 
+import com.sun.javafx.runtime.SystemProperties;
 import enumeration.EnumClaseLicencia;
 import hibernate.DAO;
+import javafx.util.converter.DateStringConverter;
+import model.Licencia;
 import model.Titular;
-
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.List;
 
 public class GestorLicencia {
 
@@ -25,11 +30,51 @@ public class GestorLicencia {
      */
     public ArrayList<EnumClaseLicencia> getClasesLicencias(Integer idTitular){
 
-        ArrayList<EnumClaseLicencia> licencias = new ArrayList<EnumClaseLicencia>();
+        ArrayList<EnumClaseLicencia> Claseslicencias = new ArrayList<EnumClaseLicencia>();
+        ArrayList<Licencia> historialLicencias = new ArrayList<Licencia>();
         Titular titular = (Titular)DAO.get().get(Titular.class, idTitular);
+        Boolean claseLicencia = true;
+
+        //retorna el historial de licencias obtenidad/actuales del titular actual
+        historialLicencias = GestorTitular.getHistorialLicencias(idTitular);
+
+        //Instancia auxiliar de licencia
+        Licencia licencia = new Licencia();
+
+        //Se asume que todos los titulares registrados tienen por lo menos 17 anos, sino no se hubiese registrado
+
+        //Clase A --> valida si esta en condiciones de solicitar una licencia clase A
+        for(int l = 0; l < historialLicencias.size(); l++) {
+                licencia = historialLicencias.get(l);
+                if(licencia.getClaseLicencia().equals(EnumClaseLicencia.CLASE_A)){
+                if(licencia.getFechaVencimiento().isAfter(LocalDate.now())){
+                    claseLicencia = false;
+                    break;
+                }
+            }
+        }
+        if(claseLicencia) Claseslicencias.add(EnumClaseLicencia.CLASE_A);
+        else claseLicencia = true;
+
+        //Clase B --> se valida si esta en condiciones de solicitar una licencia clase B
+        for(int l = 0; l < historialLicencias.size(); l++) {
+            licencia = historialLicencias.get(l);
+            if(licencia.getClaseLicencia().equals(EnumClaseLicencia.CLASE_B) ||
+               licencia.getClaseLicencia().equals(EnumClaseLicencia.CLASE_C)){
+                if(licencia.getFechaVencimiento().isAfter(LocalDate.now())){
+                    claseLicencia = false;
+                    break;
+                }
+            }
+        }
+        if(claseLicencia) Claseslicencias.add(EnumClaseLicencia.CLASE_A);
+        else claseLicencia = true;
 
 
-        return licencias;
+
+
+
+        return Claseslicencias;
     }
 
     public double calcularCostoLicencia(){
--- src/main/java/gestor/GestorTitular.java
+++ src/main/java/gestor/GestorTitular.java
@@ -5,8 +5,10 @@ import hibernate.DAO;
 import model.Licencia;
 import model.Titular;
 
+import java.lang.reflect.Array;
 import java.time.LocalDate;
 import java.time.Period;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
@@ -39,8 +41,8 @@ public class GestorTitular {
         return  dto;
     }
 
-    public List<Licencia> getHistorialLicencias(Integer idTitular){
-        return (List<Licencia>) DAO.get().getResultList("select * from licencia where id_titular="+idTitular, Licencia.class);
+    public static ArrayList<Licencia> getHistorialLicencias(Integer idTitular){
+        return (ArrayList<Licencia>) DAO.get().getResultList("select * from licencia where id_titular="+idTitular, Licencia.class);
     }
 
     public Integer getEdad(LocalDate fechaNacimiento){
