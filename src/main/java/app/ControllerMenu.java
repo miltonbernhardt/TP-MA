05913d5dd1a5fe390dbe87d037d0c5434bdd82be
WeidmannCamila <camiweid@gmail.com>
2020-10-23 13:13:13 -0300

    chiches al Menu

--- src/main/java/app/ControllerMenu.java
+++ src/main/java/app/ControllerMenu.java
@@ -6,6 +6,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -14,8 +15,128 @@ public class ControllerMenu implements Initializable {
 
     @FXML private Button BRegTitular;
     @FXML private Button BEmitirLicencia;
-    @FXML private ImageView logoini;
-    @FXML private ImageView imagenmenu;
+
+    @FXML private AnchorPane panReq;
+    @FXML private AnchorPane panUsuarios;
+    @FXML private AnchorPane panValor;
+    @FXML private AnchorPane panVigen;
+    @FXML private ImageView arrowUser;
+    @FXML private ImageView arrowVal;
+    @FXML private ImageView arrowReq;
+    @FXML private ImageView arrowVig;
+    @FXML private ImageView santaFe;
+    @FXML private AnchorPane panRecuerda;
+    @FXML private ImageView arrowRecuerda;
+
+    public void onRecuerdaButtonClicked(MouseEvent event){
+        if (panRecuerda.isVisible()){
+            panRecuerda.setVisible(false);
+            santaFe.setVisible(true);
+            arrowRecuerda.setVisible(false);
+            return;
+
+        }
+        panRecuerda.setVisible(true);
+        arrowRecuerda.setVisible(true);
+
+        panRecuerda.setVisible(true);
+        arrowRecuerda.setVisible(true);
+        santaFe.setVisible(false);
+        arrowUser.setVisible(false);
+        arrowVal.setVisible(false);
+        arrowReq.setVisible(false);
+        panUsuarios.setVisible(false);
+        panReq.setVisible(false);
+        panValor.setVisible(false);
+        //AcademPanel.setVisible(false);
+    }
+
+    public void onVigButtonClicked(MouseEvent event){
+        if (panVigen.isVisible()){
+            panVigen.setVisible(false);
+            santaFe.setVisible(true);
+            arrowVig.setVisible(false);
+            return;
+
+        }
+        panRecuerda.setVisible(false);
+        arrowRecuerda.setVisible(false);
+        panVigen.setVisible(true);
+        arrowVig.setVisible(true);
+        santaFe.setVisible(false);
+        arrowUser.setVisible(false);
+        arrowVal.setVisible(false);
+        arrowReq.setVisible(false);
+        panUsuarios.setVisible(false);
+        panReq.setVisible(false);
+        panValor.setVisible(false);
+        //AcademPanel.setVisible(false);
+    }
+    public void onValorButtonClicked(MouseEvent event){
+        if (panValor.isVisible()){
+            panValor.setVisible(false);
+            arrowVal.setVisible(false);
+            santaFe.setVisible(true);
+            return;
+
+        }
+        panValor.setVisible(true);
+        arrowVal.setVisible(true);
+        panRecuerda.setVisible(false);
+        arrowRecuerda.setVisible(false);
+        santaFe.setVisible(false);
+        arrowUser.setVisible(false);
+        panUsuarios.setVisible(false);
+        panReq.setVisible(false);
+        panVigen.setVisible(false);
+        arrowVig.setVisible(false);
+        arrowReq.setVisible(false);
+        //AcademPanel.setVisible(false);
+    }
+    public void onReqButtonClicked(MouseEvent event){
+        if (panReq.isVisible()){
+            panReq.setVisible(false);
+            arrowReq.setVisible(false);
+            santaFe.setVisible(true);
+            return;
+
+        }
+        panReq.setVisible(true);
+        arrowReq.setVisible(true);
+        santaFe.setVisible(false);
+        arrowVal.setVisible(false);
+        arrowUser.setVisible(false);
+        panRecuerda.setVisible(false);
+        arrowRecuerda.setVisible(false);
+        panUsuarios.setVisible(false);
+        panValor.setVisible(false);
+        panVigen.setVisible(false);
+        arrowVig.setVisible(false);
+        //AcademPanel.setVisible(false);
+    }
+
+    public void onUserButtonClicked(MouseEvent event){
+        if (panUsuarios.isVisible()){
+            panUsuarios.setVisible(false);
+           arrowUser.setVisible(false);
+            santaFe.setVisible(true);
+            return;
+        }
+        panUsuarios.setVisible(true);
+        arrowUser.setVisible(true);
+        panVigen.setVisible(false);
+        santaFe.setVisible(false);
+        arrowReq.setVisible(false);
+        arrowVal.setVisible(false);
+        arrowVig.setVisible(false);
+        panReq.setVisible(false);
+        panValor.setVisible(false);
+        panRecuerda.setVisible(false);
+        arrowRecuerda.setVisible(false);
+
+
+    }
+
 
     @FXML
     private void darAltaTitular() {
