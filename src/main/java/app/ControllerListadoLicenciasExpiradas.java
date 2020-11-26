9665d0361816812c0ca2160672cd2d100ad686a8
MatiasWettstein <matiasjwettstein@gmail.com>
2020-11-26 17:35:45 -0300

    inicializados los campos de la pantalla y creado los controles para que se ingresen los tipos de caracteres permitidos

--- src/main/java/app/ControllerListadoLicenciasExpiradas.java
+++ src/main/java/app/ControllerListadoLicenciasExpiradas.java
@@ -1,14 +1,23 @@
 package app;
 
-import dto.DTOAltaTitular;
+
+import dto.DTOLicenciaExpirada;
+
+import enumeration.EnumClaseLicencia;
 import enumeration.EnumTipoDocumento;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextField;
+import java.net.URL;
 
-public class ControllerListadoLicenciasExpiradas {
+import java.util.ResourceBundle;
+
+public class ControllerListadoLicenciasExpiradas implements Initializable {
     private static ControllerListadoLicenciasExpiradas instance = null;
     //private DTOAltaTitular dto = new DTOAltaTitular();
 
@@ -26,7 +35,7 @@ public class ControllerListadoLicenciasExpiradas {
     @FXML private TextField campoDoc;
     @FXML private DatePicker campoFechaInicial;
     @FXML private DatePicker campoFechaFinal;
-    @FXML private ComboBox<EnumTipoDocumento> CBClaseLicencia;
+    @FXML private ComboBox<EnumClaseLicencia> CBClaseLicencia;
     @FXML private TextField campoNroLicencia;
     @FXML private CheckBox filtrarPorRangoFecha;
     @FXML private CheckBox ordenarDesc;
@@ -36,16 +45,89 @@ public class ControllerListadoLicenciasExpiradas {
         ControllerApp.getViewAnterior();
         instance = null;
     }
-
     @FXML
     public void onCheckRangoClick(){
         if (filtrarPorRangoFecha.isSelected()){
-           campoFechaFinal.getEditor().setDisable(false);
+           campoFechaFinal.setDisable(false);
         }
-        else campoFechaFinal.getEditor().setDisable(true);
+        else {
+            campoFechaFinal.setDisable(true);
+            campoFechaFinal.getEditor().setText("");
+        }
     }
 
 
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        campoFechaFinal.getEditor().setDisable(true);
+        campoFechaInicial.getEditor().setDisable(true);
+
+        campoDoc.textProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observable, String oldValue,
+                                String newValue) {
+                if (!newValue.matches("^[0-9A-Za-z]*")) {
+                    String s = campoDoc.getText().substring(0,campoDoc.getText().length()-1);
+                    campoDoc.setText(s);
+                }
+            }
+        });
+        campoApe.textProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observable, String oldValue,
+                                String newValue) {
+                if (!newValue.matches("[^0-9]*")) {
+                    String s = campoApe.getText().substring(0,campoApe.getText().length()-1);
+                    campoApe.setText(s);
+                }
+            }
+        });
+
+        campoNombre.textProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observable, String oldValue,
+                                String newValue) {
+                if (!newValue.matches("[^0-9]*")) {
+                    String s = campoNombre.getText().substring(0,campoNombre.getText().length()-1);
+                    campoNombre.setText(s);
+                }
+            }
+        });
+        campoNroLicencia.textProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observable, String oldValue,
+                                String newValue) {
+                if (!newValue.matches("\\d*")) {
+                    String s = campoNroLicencia.getText().substring(0,campoNroLicencia.getText().length()-1);
+                    campoNroLicencia.setText(s);
+                }
+            }
+        });
+
+        CBClaseLicencia.setPromptText("Clase");
+        CBClaseLicencia.getItems().setAll(EnumClaseLicencia.values());
+
+        CBTipoDNI.setPromptText("Tipo Doc.");
+        CBTipoDNI.getItems().setAll(EnumTipoDocumento.values());
 
 
+    }
+
+    public void buscarLicencias(){
+//        private EnumTipoDocumento tipoDNI;
+//        private String DNI;
+
+//        private Integer nroLicencia;
+//        private EnumClaseLicencia claseLicencia;
+//        private LocalDate fechaInicial;
+//        private LocalDate fechaFinal;
+//        private boolean ordenamientoDescendente;
+
+        DTOLicenciaExpirada DTOLE = new DTOLicenciaExpirada();
+        DTOLE.setApellido(campoApe.getText());
+        DTOLE.setNombre(campoNombre.getText());
+       // DTOLE.
+
+
+    }
 }
