43be734bcfe57be0c9895ac47e31136ffc087dbe
IndianaLozano <IndianaLozano>
2020-11-29 13:01:43 -0300

    Agregados casos de prueba para calcularVigencia.

--- src/test/java/TestGestorLicencia.java
+++ src/test/java/TestGestorLicencia.java
@@ -10,8 +10,7 @@ import java.time.format.DateTimeFormatter;
 public class TestGestorLicencia {
 
     @Test
-    public void calcularVigencia() throws MenorDeEdadException {
-
+    public void calcularVigencia_1anio() throws MenorDeEdadException {
         LocalDate nacimiento = LocalDate.parse("25-06-2002", DateTimeFormatter.ofPattern("dd-MM-yyyy"));
         LocalDate vencimiento = LocalDate.parse("25-06-2021", DateTimeFormatter.ofPattern("dd-MM-yyyy"));
         Vigencia vigencia = GestorLicencia.calcularVigencia(nacimiento, 0);
@@ -19,6 +18,27 @@ public class TestGestorLicencia {
         Assert.assertEquals(vigencia.getVigencia(), 1);
         System.out.println(vigencia.getFechaVencimiento());
         Assert.assertEquals(vigencia.getFechaVencimiento(), vencimiento);
+    }
 
+    @Test
+    public void calcularVigencia_3anios() throws MenorDeEdadException {
+        LocalDate nacimiento = LocalDate.parse("21-03-2000", DateTimeFormatter.ofPattern("dd-MM-yyyy"));
+        LocalDate vencimiento = LocalDate.parse("21-03-2023", DateTimeFormatter.ofPattern("dd-MM-yyyy"));
+        Vigencia vigencia = GestorLicencia.calcularVigencia(nacimiento, 100);
+        System.out.println(vigencia.getVigencia());
+        Assert.assertEquals(vigencia.getVigencia(), 3);
+        System.out.println(vigencia.getFechaVencimiento());
+        Assert.assertEquals(vigencia.getFechaVencimiento(), vencimiento);
+    }
+
+    @Test
+    public void calcularVigencia_5anios() throws MenorDeEdadException {
+        LocalDate nacimiento = LocalDate.parse("08-07-1996", DateTimeFormatter.ofPattern("dd-MM-yyyy"));
+        LocalDate vencimiento = LocalDate.parse("08-07-2025", DateTimeFormatter.ofPattern("dd-MM-yyyy"));
+        Vigencia vigencia = GestorLicencia.calcularVigencia(nacimiento, 111);
+        System.out.println(vigencia.getVigencia());
+        Assert.assertEquals(vigencia.getVigencia(), 5);
+        System.out.println(vigencia.getFechaVencimiento());
+        Assert.assertEquals(vigencia.getFechaVencimiento(), vencimiento);
     }
 }
