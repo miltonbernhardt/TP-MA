c88624e8896e5111251bf57afbf200a71161d618
WeidmannCamila <camiweid@gmail.com>
2020-11-30 09:40:32 -0300

    Primeros pasos Imprimir

--- src/main/java/app/ControllerImprimirLicencia.java
+++ src/main/java/app/ControllerImprimirLicencia.java
@@ -1,8 +1,111 @@
 package app;
 
+import com.itextpdf.text.*;
+import com.itextpdf.*;
+import com.itextpdf.text.Image;
+import com.itextpdf.text.pdf.PdfWriter;
+
+import com.pdfjet.*;
+import javafx.embed.swing.SwingFXUtils;
 import javafx.fxml.FXML;
+import javafx.scene.SnapshotParameters;
+import javafx.scene.control.Alert;
+import javafx.scene.image.WritableImage;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.FileChooser;
+
+import javax.imageio.ImageIO;
+import javax.swing.filechooser.FileSystemView;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.*;
 
 public class ControllerImprimirLicencia {
+
+    @FXML private AnchorPane panel;
+
+    private static ControllerImprimirLicencia instance = null;
+
+
+    public static ControllerImprimirLicencia get() {
+        if (instance == null){
+            ControllerApp.setViewAnterior();
+            instance = (ControllerImprimirLicencia) ControllerApp.setRoot("imprimirLic", "Licencia");
+        }
+        return instance;
+    }
+    public void imprimirLicencia() throws FileNotFoundException, DocumentException {
+        FileChooser fc= new FileChooser();
+        try{
+        fc.getExtensionFilters().add(new FileChooser.ExtensionFilter("PDF File", "*.pdf"));
+        fc.setTitle("Save to pdf");
+        fc.setInitialFileName("untitled.pdf");
+        Alert alert=null;
+            //alert = new Alert(Alert.AlertType.ERROR);
+        File file = fc.showSaveDialog(null);
+        String str = file.getAbsolutePath();
+        FileOutputStream fos = new FileOutputStream(str);
+        Document pdf = new Document();
+            PdfWriter.getInstance(pdf, fos);
+            pdf.open();
+        // Page page = new Page(pdf, Letter.LANDSCAPE);
+            File fileJ = new File("..TP-MA/temp/foto.png");
+            //exportFileChooser.showSaveDialog(alert.getOwner());
+            fileJ.getParentFile().mkdirs();//crete temp directory if not available
+            final SnapshotParameters spa = new SnapshotParameters();
+            spa.setTransform(javafx.scene.transform.Transform.scale(0.71, 1.1));//Scaling only if required i.e., the screenshot is unable to fit in the page
+            WritableImage image = panel.snapshot(spa, null);//This part takes the snapshot, here node is the anchorpane you sent
+            BufferedImage buffImage = SwingFXUtils.fromFXImage(image, null);
+            ImageIO.write(buffImage, "png", fileJ);//Writes the snapshot to file
+
+
+         ///   BufferedImage bufferedImage = null;
+
+        //    bufferedImage = ImageIO.read(new ByteArrayInputStream(Document pdf));
+
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            ImageIO.write(buffImage, "png", baos);
+            Image iTextImage = Image.getInstance(baos.toByteArray());
+
+           pdf.add(iTextImage);
+
+
+            pdf.close();
+            fos.flush();
+            Desktop.getDesktop().open(file);
+
+       /* PdfWriter.getInstance(document, file);
+
+        //document.open();
+          //  document.add(new Paragraph("Esto es el primer párrafo, normalito"));
+        //Alert alert=null;
+        //alert = new Alert(Alert.AlertType.ERROR);
+        //FileChooser exportFileChooser = new FileChooser(); //The folder selector to save the pdf
+       // exportFileChooser.setInitialDirectory(FileSystemView.getFileSystemView().getDefaultDirectory());
+        //exportFileChooser.setInitialFileName("HOLI");
+        File fileJ = new File("../desktop/HOLI.png");
+        //exportFileChooser.showSaveDialog(alert.getOwner());
+        fileJ.getParentFile().mkdirs();//crete temp directory if not available
+        final SnapshotParameters spa = new SnapshotParameters();
+        spa.setTransform(javafx.scene.transform.Transform.scale(0.71, 1.1));//Scaling only if required i.e., the screenshot is unable to fit in the page
+        WritableImage image = panel.snapshot(spa, null);//This part takes the snapshot, here node is the anchorpane you sent
+        BufferedImage buffImage = SwingFXUtils.fromFXImage(image, null);
+        ImageIO.write(buffImage, "png", fileJ);//Writes the snapshot to file
+        document.add((Element) fileJ);
+        document.close();
+        file.close();*/
+        }catch (Exception e) {
+        e.printStackTrace();
+        }
+
+        /*try{
+         //   ImageIO.write(buffImage, "png", file);//Writes the snapshot to file
+            File file = new File("C:/impresiones/archiv.pdf");
+            Desktop.getDesktop().open(file);
+        }catch (Exception e) {
+            e.printStackTrace();
+        }*/
+    }
     /*@FXML
     private void ButtonAction(ActionEvent event)throws IOException, DocumentException {
 
