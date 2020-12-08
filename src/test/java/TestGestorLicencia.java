360a986b04af048ef47f75c42a5e3e58df9fe967
IndianaLozano <IndianaLozano>
2020-12-08 18:39:11 -0300

    TestGestorLicencia.calcularVigencia_3anios() : falta que el save devuelva el id del titular

--- src/test/java/TestGestorLicencia.java
+++ src/test/java/TestGestorLicencia.java
@@ -1,26 +1,51 @@
+import database.TitularDAO;
+import database.TitularDAOImpl;
 import dto.DTOAltaTitular;
 import dto.DTOEmitirLicencia;
-import enumeration.EnumClaseLicencia;
-import enumeration.EnumTipoDocumento;
+import enumeration.*;
 import exceptions.MenorDeEdadException;
 import gestor.GestorLicencia;
 import gestor.GestorTitular;
+import model.Licencia;
+import model.Titular;
 import model.Vigencia;
 import org.junit.Test;
 import org.junit.Assert;
 
 import java.time.LocalDate;
+import java.time.Month;
 import java.time.format.DateTimeFormatter;
 
 public class TestGestorLicencia {
 
     @Test
-    public void calcularVigencia_1anio() {
-        LocalDate nacimiento = LocalDate.parse("08-08-2001", DateTimeFormatter.ofPattern("dd-MM-yyyy"));
-        LocalDate vencimiento = LocalDate.parse("08-08-2021", DateTimeFormatter.ofPattern("dd-MM-yyyy"));
-        Vigencia vigencia = GestorLicencia.calcularVigencia(nacimiento, 141);
+    public void calcularVigencia_3anios() {
+        Titular t1 = new Titular(EnumTipoDocumento.DNI,
+                "42000001",
+                "PruebaTipoB",
+                "caseB1",
+                LocalDate.of(2000, Month.MARCH, 21),
+                EnumGrupoSanguineo.GRUPO_0,
+                EnumFactorRH.FACTOR_POSITIVO,
+                true, EnumSexo.MASCULINO);
+        Licencia l1 = new Licencia(t1,
+                EnumClaseLicencia.CLASE_B,
+                LocalDate.of(2018, Month.MARCH, 21),
+                LocalDate.of(2019, Month.MARCH, 21));
+        //Agrego licencia al titular
+        t1.getLicencias().add(l1);
+        //t1.getLicencias().add(l2);
+        //Persisto en Base de Datos
+        long id = -1;
+        try {
+            id = dao.save(t1);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        Vigencia vigencia = GestorLicencia.calcularVigencia(t1.getFechaNacimiento(), id);
+        LocalDate vencimiento = LocalDate.parse("21-03-2022", DateTimeFormatter.ofPattern("dd-MM-yyyy"));
         System.out.println(vigencia.getVigencia());
-        Assert.assertEquals(vigencia.getVigencia(), 1);
+        Assert.assertEquals(vigencia.getVigencia(), 3);
         System.out.println(vigencia.getFechaVencimiento());
         Assert.assertEquals(vigencia.getFechaVencimiento(), vencimiento);
     }
